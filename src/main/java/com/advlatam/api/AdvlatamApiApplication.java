c645291b96e59841341a9e63226153e5be95feb8
Miguel Torres <miguetv17@gmail.com>
2020-02-23 17:28:55 -0500

    wip

--- src/main/java/com/advlatam/api/AdvlatamApiApplication.java
+++ src/main/java/com/advlatam/api/AdvlatamApiApplication.java
@@ -3,11 +3,9 @@ package com.advlatam.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.advlatam"})
 @EntityScan(basePackages = {"com.advlatam.model"})
 @EnableJpaRepositories("com.advlatam.repositories")
 public class AdvlatamApiApplication {
--- src/test/java/com/advlatam/api/AdvlatamApiApplicationTests.java
+++ src/test/java/com/advlatam/api/AdvlatamApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.advlatam.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest // tells Spring Boot to look for a main configuration class (one with @SpringBootApplication, for instance)
+@AutoConfigureMockMvc
 class AdvlatamApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void shouldReturnNotFoundAtRoot() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isNotFound());
 	}
 
 }
